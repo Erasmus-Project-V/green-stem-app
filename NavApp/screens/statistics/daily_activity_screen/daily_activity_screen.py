f9aeb4e07f3b31772644d9a85d3473eb09b030de
Sven Matic <sven.matic6@gmail.com>
2024-04-05 23:13:44 +0200

    Creating the daily activity screen.

--- NavApp/screens/statistics/daily_activity_screen/daily_activity_screen.py
+++ NavApp/screens/statistics/daily_activity_screen/daily_activity_screen.py
@@ -2,4 +2,15 @@ from kivymd.uix.screen import MDScreen
 
 
 class DailyActivityScreen(MDScreen):
-    pass
+    def start_up_screen(self, activity_name, activity_time):
+        self.ids["activity_name"].text = activity_name
+        self.ids["activity_time"].text = activity_time
+        #self.change_labels(activity_type)
+        pass
+
+    def back_arrow(self):
+        self.manager.transition.direction = "right"
+        self.manager.goto_screen("mss")
+        # not good, should be fired on complete of transition
+        mss = self.manager.get_screen("mss")
+        # mss.bind(on_enter=mss.reenter_hero)
--- NavApp/screens/statistics/monthly_activity_screen/monthly_activity_screen.py
+++ NavApp/screens/statistics/monthly_activity_screen/monthly_activity_screen.py
@@ -2,4 +2,9 @@ from kivymd.uix.screen import MDScreen
 
 
 class MonthlyActivityScreen(MDScreen):
-    pass
+    def start_up_screen(self, hero_tag, activity_type):
+        self.ids["hero_to"].tag = hero_tag
+        self.activity = activity_type
+        print(self.activity)
+        self.ids["top_bar"].title = activity_type
+        # self.change_labels(activity_type)
