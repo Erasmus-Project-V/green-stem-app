d6994f531d17252681b0aa0e7d807b47c2f40de1
S1lentAFK <86916610+S1lentAFK@users.noreply.github.com>
2024-04-20 13:02:44 +0200

    .py File for "text_information_screen"
    male promjene u .kv file-u i dodana logika da polaći tekst sa servera

--- NavApp/screens/other/text_information_screen/text_information_screen.py
+++ NavApp/screens/other/text_information_screen/text_information_screen.py
@@ -1,16 +1,29 @@
+import requests
 from kivymd.uix.screen import MDScreen
-
 
 class TextInformationScreen(MDScreen):
 
     caller: str
-    def goto_profile(self,btn):
+    fetched = False ## Dodavanje fetched varijable tako da se text samo jednom poziva sa servera a ne x puta...
+
+    def goto_profile(self, btn):
         self.manager.goto_screen("pfs")
 
     def start_up_screen(self):
-        # ovo se zove automatski na ulasku u ekran
-        # ova funkcija treba populirati labele sa pravim tekstom ovisno otkud je funkcija pozvana
-        # to će vam reći varijabla self.caller -> moguce vrijednosti (privacy -> pri, contact us -> con, points and levels - pal)
-        # za sada napravite inicijalizaciju za privacy, učitajte tekst sa interneta nekako! (good luck)
-        # SADRŽAJ MORA BITI SKROLABILAN - MDSCROLLVIEW, no nemojte slučajno da i skrola naslov i back button!!!
-        pass
+        if not TextInformationScreen.fetched: ## Provjeravanje statusa fetchanog texta
+            try:
+                url = "https://www.green-stem.eu/fitness/privacy-policy.tx"
+                response = requests.get(url)
+                if response.status_code == 200:
+                    text = response.text
+                    self.ids.privacy_paragraph.text = text
+                    TextInformationScreen.fetched = True
+                else:
+                    print("Error fetching text from server! Status code:", response.status_code)
+            except Exception as e: ## Error handeling 
+                self.ids.privacy_paragraph.text = f"Error fetching text from server! {e}"
+                print("Error fetching text from server! The file might not exist!", e)
+
+
+        else: ## Debug koji printa ako je text već bio fetchan
+            print("The text was already fetched!")
